a491dfeb1d091f53ec9da33c818808a02b6135d6
shumpe11 <62501172+shumpe11@users.noreply.github.com>
2020-09-08 02:32:32 +0900

    Add files via upload

--- assets/js/catepage.js
+++ assets/js/catepage.js
@@ -30,12 +30,17 @@ let pathObj = {
   wave:'0468/03/rainbowCube'
 }
 
-window.onload = function() {
-  const spinner = document.getElementById('loading');
+$('head').append(
+	'<style type="text/css">#container { display: none; } #fade, #loader { display: block; }</style>'
+);
 
-  // Add .loaded to .loading
-  spinner.classList.add('loaded');
-}
+jQuery.event.add(window,"load",function() { // 全ての読み込み完了後に呼ばれる関数
+	var pageH = $("#container").height();
+
+	$("#fade").css("height", pageH).delay(900).fadeOut(800);
+	$("#loader").delay(600).fadeOut(300);
+	$("#container").css("display", "block");
+});
 
 
 
