e9675409df4f2205173e61777e50b9ad7d48d006
shumpe11 <62501172+shumpe11@users.noreply.github.com>
2020-09-18 04:23:09 +0900

    Add files via upload

--- assets/js/catepage.js
+++ assets/js/catepage.js
@@ -1,8 +1,7 @@
 "use strict";
 
-let userAgent = window.navigator.userAgent;
-let browserBackEvent = 0;
 let currentWorkId = ''
+const userAgent = window.navigator.userAgent;
 const pathObj = {
 // game
 dodge:'0067/02/index',
@@ -17,6 +16,7 @@ quiz:'0009/02/index',
 fish:'0332/02/index',
 piano:'0009/01/index',
 solarsystemQuiz:'0508/02/index',
+sakegame:'0480/資料/まとめサイト/02/gameHP',
 
 // anime2d
 countdown:'0111/02/index',
@@ -26,6 +26,7 @@ clock:'0398/02/index',
 firework:'0379/02/index',
 snow:'0515/01/index',
 moya:'0475/01/index',
+fall:'0475/02/index',
 
 // anime3d
 solarsystem:'0111/01/index',
@@ -38,65 +39,59 @@ drawart:'0138/02/index',
 drawing:'0480/資料/まとめサイト/01/index(お絵描き)'
 }
 
-// ウィンドウサイズが768px以上のときアニメーション適用
+// xs以外のときアニメーション適用
 if (matchMedia('(min-width: 768px)').matches) {
 
-// モーダルウィンドウ調整
-$(".modal-body").wrapInner('<div class="row"></div>')
+  // モーダルウィンドウ調整
+  $(".modal-body").wrapInner('<div class="row"></div>')
 
-// ブラウザがEdge以外のとき
-if(userAgent.indexOf('Edge') == -1) {
-  //読み込み時にcardが落ちてくるアニメーションを付与するクラスを作品全体に設定
-  $("#workContainer").addClass("animate__animated animate__slideInDown animate__slow");
+  // ブラウザがEdge以外のとき
+  if(userAgent.indexOf('Edge') == -1) {
+    //読み込み時にcardが落ちてくるアニメーションを付与するクラスを作品全体に設定
+    $(".container").addClass("animate__animated animate__slideInDown animate__slow");
 
+    // モーダルウィンドウサイズ調整
+    $('#modal').children('.modal-dialog').addClass("modal-lg")
 
-  // モーダルウィンドウサイズ調整
-  $('#modal').children('.modal-dialog').addClass("modal-lg")
+  //アニメーション終了後、作品の説明テキストがスライド表示されるように(slideToggle)
+    setTimeout(function(){
+      $('.slideIn').slideToggle();
+      $('.moimg').removeClass('border-bottom');
+      $('header').addClass('sticky-top');
+    },2000)
 
-  //アニメーション終了後、作品の説明テキストがスライド表示されるように
-  setTimeout(function(){
-    $('.slideIn').slideToggle();
-    $('.moimg').removeClass('border-bottom');
-    $('header').addClass('sticky-top');
-  },2000)
-
-} else{
+  } else {
     // Microsoft Edgeのときデザイン調整
     $('header').addClass('sticky-top');
-    $('#lgTitle').removeClass('d-lg-block');
-    $('#mdsmTitle').removeClass('d-lg-none');
+    $('#lg-title').removeClass('d-lg-block');
+    $('#mdsm-title').removeClass('d-lg-none');
     $('#wrap').removeClass('col-lg-5');
     $('#wrap').addClass('col-lg-12');
-    $('#descriptionWrap').removeClass('col-lg-7');
-    $('#descriptionWrap').addClass('col-lg-12');
+    $('#modal-body-description-wrap').removeClass('col-lg-7');
+    $('#modal-body-description-wrap').addClass('col-lg-12');
     $('.card-text').css('border-bottom', '1px solid #fff');
   }
-} else{
+} else {
+  // xsは固定ヘッダーのみ
   $('header').addClass('sticky-top');
 }
 
 
 $('.navbar-toggler').on('click', function() {
-$('.nav-link').addClass("animate__animated animate__fadeInUp");
+  $('.nav-link').addClass("animate__animated animate__fadeInUp");
 });
 
 if(userAgent.indexOf('Edge') == -1) {
   $('#modal').on('hidden.bs.modal', function () {
-    // do something…
-    // console.log(currentWorkId);
-    $("#" + currentWorkId).removeClass("animate__animated animate__fadeOutUp");
-    $("#" + currentWorkId).addClass("animate__animated animate__slideInDown animate__fast");
+    // モーダルが閉じられたときのアニメーション
+  $("#" + currentWorkId).removeClass("animate__animated animate__fadeOutUp");
+  $("#" + currentWorkId).addClass("animate__animated animate__fadeInDown animate__fast");
     setTimeout(function(){
-      $("#" + currentWorkId).removeClass("animate__animated animate__slideInDown");
+      $("#" + currentWorkId).removeClass("animate__animated animate__fadeInDown");
       $('header').addClass('sticky-top');
-
     },500)
-
   })
 }
-
-
-
 
 
 $('.work-wrapper').on('click', function() {
@@ -109,7 +104,8 @@ let currentHandlename = $("#"+currentWorkId).find('.text-muted')[0].outerText;
 if(userAgent.indexOf('Edge') == -1) {
   $('header').removeClass('sticky-top');
   $("#" + currentWorkId).addClass("animate__animated animate__fadeOutUp animate__fast");
-  
+  // $(".slideIn").slideToggle();
+
   $('[data-toggle=modal]').on('click', function (e) {
     let $target = $($(this).data('target'));
     $target.data('triggered',true);
@@ -123,10 +119,10 @@ if(userAgent.indexOf('Edge') == -1) {
   });
 }
 
-$("#modalGif").attr('src','assets/img/' + currentWorkId + '.gif');
+$("#modal-gif").attr('src','assets/img/' + currentWorkId + '.gif');
 $(".modal-body-title")[0].textContent = modalTitle;
 $(".modal-body-title")[1].textContent = modalTitle;
-$("#modalDescription")[0].textContent = modalText;
+$("#modal-body-description")[0].textContent = modalText;
 $("#handlename")[0].textContent = currentHandlename;
 
 $("#show-detail").attr('href','work/' + currentPath + '.html');
